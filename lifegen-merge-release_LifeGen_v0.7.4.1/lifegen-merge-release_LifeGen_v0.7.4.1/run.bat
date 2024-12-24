87aeaae34d9e6d6a996240aa127e7d008e561eea
Rowannothere <rowan.isnt.here@gmail.com>
2024-12-24 12:02:20 -0500

    Revert "the entire fucking thing"
    This reverts commit d09960ed5b3df6d28d012c33f3c0ca264046dad1.

--- lifegen-merge-release_LifeGen_v0.7.4.1/lifegen-merge-release_LifeGen_v0.7.4.1/run.bat
+++
@@ -1,2 +0,0 @@
-%AppData%\Python\Scripts\poetry install --no-root
-%AppData%\Python\Scripts\poetry run python main.py
--- lifegen-merge-release_LifeGen_v0.7.4.1/lifegen-merge-release_LifeGen_v0.7.4.1/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-poetry install --no-root
-poetry run python main.py
